6ccda6b4560927ba4471db1737ecc1fdbd9d64b9
Göktürk ŞİMŞEK <91979860+gokturkx@users.noreply.github.com>
2021-11-23 12:18:12 +0300

    dosyalar eklendi

---
+++ eğitim siteleri.bat
@@ -0,0 +1,9 @@
+echo off
+start msedge https://www.udemy.com/home/my-courses/learning/
+start msedge https://www.btkakademi.gov.tr/portal/catalog
+start msedge https://globalenstitu.com/ucretsiz-uzaktan-egitim-sertifika-programlari/#login
+start msedge https://www.iienstitu.com/login?next=https%3A%2F%2Fwww.iienstitu.com%2Fprofil
+start msedge https://bilgeis.net/moodle/my/
+start https://www.designfreelogoonline.com/
+start https://www.discudemy.com/category
+echo
